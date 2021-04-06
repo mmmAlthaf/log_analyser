8f0095ee47044ac43b767556c91850029314f543
mmmAlthaf <mw.althaf@gmail.com>
2021-04-06 11:20:09 +0530

    simplified a bit

--- analyse.py
+++ analyse.py
@@ -2,62 +2,70 @@ import os
 import re
 import sys
 
+
 inputFile = sys.argv[1]
-print("What analysis do you want?\n1) Logs with key word\n2) With Time stamp\n3) Every action after user log in")
-choice = input('>')
 
-if(choice == "1"):
-    keyWord = input('Keyword: ')
-    line_regex = re.compile(r".*"+keyWord+".*$")
-    output_filename = os.path.normpath("output.log")
-    with open(output_filename, "w") as out_file:
-        out_file.write("")
+# emptying the output.log file
+output_filename = os.path.normpath("output.log")
+with open(output_filename, "w") as out_file:
+    out_file.write("")
 
+#looping through the log with a regex and writes to the output.log file
+def findLogs(line_regex):
     with open(output_filename, "a") as out_file:
         with open(inputFile, "r") as in_file:
             for line in in_file:
                 if (line_regex.search(line)):
                     # print(line)
                     out_file.write(line)
-    print("Find the analysis in file output.log")
+
+print("What analysis do you want?\n1) Action Logs\n2) LANG SERVER logs\n3) Every action after USER LOGIN")
+choice = input('>')
+
+if(choice == "1"):
+    line_regex = re.compile(r".*action.*$")
+    findLogs(line_regex)
 
 if(choice == "2"):
-    keyWord = input('Keyword (leave blank if not needed): ')
-    startTime = input('Start Time (yy-mm-ddThh:mm:ss): ')
-    endTime = input('End Time (yy-mm-ddThh:mm:ss): ')
-    line_regex = re.compile(r".*"+keyWord+".*$")
-    output_filename = os.path.normpath("output.log")
-    with open(output_filename, "w") as out_file:
-        out_file.write("")
-    isBetweenTime = False
-    with open(output_filename, "a") as out_file:
-        with open(inputFile, "r") as in_file:
-            for line in in_file:
-                if startTime in line:
-                    isBetweenTime = True
-                if(isBetweenTime):
-                    if (line_regex.search(line)):
-                        # print(line)
-                        out_file.write(line)
-                if endTime in line:
-                    isBetweenTime = False
-    print("Find the analysis in file output.log")
+    line_regex = re.compile(r".*LANG CLIENT.*$")
+    findLogs(line_regex)
 
 if(choice == "3"):
-    keyWord = "%c action"
-    line_regex = re.compile(r".*"+keyWord+".*$")
-    output_filename = os.path.normpath("output.log")
-    with open(output_filename, "w") as out_file:
-        out_file.write("")
+    line_regex = re.compile(r".*action.*$")
     checkLogin = False
     with open(output_filename, "a") as out_file:
         with open(inputFile, "r") as in_file:
             for line in in_file:
-                if(line.contains("USER_LOGIN")):
+                if "LOGIN_START" in line:
                     checkLogin = True
                 if(checkLogin):
                     if (line_regex.search(line)):
                         # print(line)
                         out_file.write(line)
-    print("Find the analysis in file output.log")
 
+
+print("Find the analysis in file output.log")
+
+# Still testing this out
+#
+# if(choice == "4"):
+#     keyWord = input('Keyword (leave blank if not needed): ')
+#     startTime = input('Start Time (yy-mm-ddThh:mm:ss): ')
+#     endTime = input('End Time (yy-mm-ddThh:mm:ss): ')
+#     line_regex = re.compile(r".*"+keyWord+".*$")
+#     output_filename = os.path.normpath("output.log")
+#     with open(output_filename, "w") as out_file:
+#         out_file.write("")
+#     isBetweenTime = False
+#     with open(output_filename, "a") as out_file:
+#         with open(inputFile, "r") as in_file:
+#             for line in in_file:
+#                 if startTime in line:
+#                     isBetweenTime = True
+#                 if(isBetweenTime):
+#                     if (line_regex.search(line)):
+#                         # print(line)
+#                         out_file.write(line)
+#                 if endTime in line:
+#                     isBetweenTime = False
+#     print("Find the analysis in file output.log")
